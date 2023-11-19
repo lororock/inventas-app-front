ba093df45f12b1c0eba4dffa816e4164ff984ea7
Ernesto Diaz <carlos.diaz@miliopay.com>
2023-11-19 13:26:46 -0500

    :sparkles: change styles for form

--- src/components/enterprises/EnterpriseForm.vue
+++ src/components/enterprises/EnterpriseForm.vue
@@ -5,42 +5,64 @@ defineProps({
 </script>
 
 <template>
-  <div class="max-w-2xl p-4 mx-auto">
-    <div class="text-gray-50 text-3xl pb-5">Datos de la empresa</div>
-    <div>
-      <label for="name" class="block text-sm font-medium text-gray-200"
+  <form class="max-w-2xl p-4 mx-auto">
+    <div class="text-center text-gray-50 text-3xl pb-5">
+      Datos de la empresa
+    </div>
+    <div class="relative z-0 w-full mb-6 group">
+      <input
+        v-model="formData.name"
+        class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+        required
+      />
+      <label
+        class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
         >Nombre</label
       >
-      <input
-        type="text"
-        v-model="formData.name"
-        id="name"
-        class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-      />
     </div>
-
-    <div>
-      <label for="name" class="block text-sm font-medium text-gray-200"
-        >Email</label
-      >
+    <div class="relative z-0 w-full mb-6 group">
       <input
-        type="text"
+        type="email"
         v-model="formData.email"
-        id="name"
-        class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+        class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+        required
       />
-    </div>
-
-    <div>
-      <label for="name" class="block text-sm font-medium text-gray-200"
-        >Tipo de documento</label
+      <label
+        class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+        >Correo electrónico</label
       >
-      <select v-model="formData.documentType">
-        <option :value="0">C.C.</option>
-        <option :value="1">C.E.</option>
-        <option :value="2">Nit</option>
-        <option :value="3">Otro</option>
-      </select>
     </div>
-  </div>
+    <div class="grid md:grid-cols-2 md:gap-6">
+      <div class="relative z-0 w-full mb-6 group">
+        <label
+          for="countries"
+          class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >Select your country</label
+        >
+        <select
+          v-model="formData.documentType"
+          class="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer"
+          required
+        >
+          <option :value="0">C.C.</option>
+          <option :value="1">C.E.</option>
+          <option :value="2">Nit</option>
+          <option :value="3">Otro</option>
+        </select>
+      </div>
+      <div class="relative z-0 w-full mb-6 group">
+        <input
+          type="text"
+          v-model="formData.documentNumber"
+          class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+          required
+        />
+        <label
+          for="floating_first_name"
+          class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >Tipo de documento</label
+        >
+      </div>
+    </div>
+  </form>
 </template>
