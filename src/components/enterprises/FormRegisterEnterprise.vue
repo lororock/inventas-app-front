c3d3dae089b0b7d137cfd3f11dc6c2754e9c1d11
Ernesto Diaz <carlos.diaz@miliopay.com>
2023-11-19 11:41:02 -0500

    :sparkles: desacoplate logic in component for form enterprise

---
+++ src/components/enterprises/FormRegisterEnterprise.vue
@@ -0,0 +1,73 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const formData = ref({
+  name: "",
+  email: "",
+  documentType: null,
+  documentNumber: "",
+  user: {
+    email: "",
+    password: "",
+    first_name: "",
+    last_name: "",
+    documentType: null,
+    documentNumber: "",
+    phone: "",
+    gender: null,
+    birthdate: "",
+    roles: [],
+  },
+});
+
+const submitForm = () => {
+  console.log("Form Data:", formData.value);
+};
+</script>
+
+<template>
+  <div class="bg-gray-900">
+    <div class="max-w-2xl p-4 mx-auto">
+      <div class="text-gray-50 text-3xl pb-5">Formulario registro empresa</div>
+      <form @submit.prevent="submitForm" class="space-y-4">
+        <div>
+          <label for="name" class="block text-sm font-medium text-gray-200"
+            >Nombre</label
+          >
+          <input
+            type="text"
+            v-model="formData.name"
+            id="name"
+            class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+          />
+        </div>
+
+        <div>
+          <label for="name" class="block text-sm font-medium text-gray-200"
+            >Email</label
+          >
+          <input
+            type="text"
+            v-model="formData.name"
+            id="name"
+            class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+          />
+        </div>
+        <!-- Repetir para otros campos... -->
+
+        <div>
+          <button
+            type="submit"
+            class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<style>
+/* Aquí puedes agregar estilos adicionales si es necesario */
+</style>
