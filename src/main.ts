535f9628bc5ef9a3447e43ebe008d5b3667244a6
Ernesto Diaz <carlos.diaz@miliopay.com>
2023-11-19 19:42:21 -0500

    :heavy_plus_sign: install vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,25 @@
 import { createApp } from "vue";
-import './assets/style.css'
+import "./assets/style.css";
 import App from "./App.vue";
 import { createPinia } from "pinia";
 import router from "./router";
 
+// Vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
 const app = createApp(App);
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
 const pinia = createPinia();
 
 app.use(pinia);
 app.use(router);
+app.use(vuetify);
 app.mount("#app");
