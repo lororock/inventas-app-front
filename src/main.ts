cca0cd70c71ce691016ac7c4a5d089d20183be95
Ernesto Diaz <carlos.diaz@miliopay.com>
2023-11-21 17:36:46 -0500

    :wrench: config vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-import { createApp } from "vue";
 import "./assets/style.css";
+import "vuetify/styles";
+import "@mdi/font/css/materialdesignicons.css";
+import { createApp } from "vue";
 import App from "./App.vue";
 import { createPinia } from "pinia";
 import router from "./router";
 
-// Vuetify
-import "vuetify/styles";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
@@ -13,13 +13,16 @@ import * as directives from "vuetify/directives";
 const app = createApp(App);
 
 const vuetify = createVuetify({
+  icons: {
+    defaultSet: "mdi",
+  },
   components,
   directives,
 });
 
 const pinia = createPinia();
 
+app.use(vuetify);
 app.use(pinia);
 app.use(router);
-app.use(vuetify);
 app.mount("#app");
