17798a333ff256e31fa21d607c1be95e8ebfece8
ernesto diaz <ernesto.diaz@miliopay.com>
2023-11-27 13:37:03 -0500

    :sparkles: create new view for recommendation after login in app with details for list

--- src/assets/list.items.ts
+++ src/assets/list.items.ts
@@ -90,3 +90,19 @@ export const functionsDashboard = [
     ]
   },
 ];
+
+export const recommendationsFormDashboard = [
+  "No compartir contraseñas entre usuarios.",
+  "Cada usuario debe tener una cuenta individual.",
+  "Desactivar cuentas de empleados que ya no estén activos.",
+  "Utilizar contraseñas fuertes y cambiarlas regularmente.",
+  "Habilitar autenticación de dos factores para mayor seguridad.",
+  "Realizar auditorías de seguridad periódicas.",
+  "Restringir el acceso a información sensible solo a quienes lo necesiten.",
+  "Mantener el software de la plataforma actualizado para protegerse contra vulnerabilidades.",
+  "Capacitar a los usuarios en buenas prácticas de seguridad.",
+  "Realizar copias de seguridad de los datos importantes de manera regular.",
+  "Evitar operar sobre productos o servicios que estén inactivos.",
+  "Desactivar entidades inactivas para mantener la integridad de la plataforma.",
+  "Registrar cada movimiento realizado en la plataforma, con acceso restringido al administrador o responsable del sistema."
+]
---
+++ src/views/Dashboard/HomeDashboard.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+import {recommendationsFormDashboard} from "../../assets/list.items.ts";
+</script>
+
+<template>
+  <v-card class="mt-2" title="Bienvenido!" subtitle="Inventas-App | Plataforma para la gestion de tu negocio">
+    <v-card-item>
+      Recomendaciones de la plataforma
+      <v-list>
+        <v-list-item v-for="(rec, index) in recommendationsFormDashboard">{{1 + index}}. {{rec}}</v-list-item>
+      </v-list>
+    </v-card-item>
+  </v-card>
+</template>
