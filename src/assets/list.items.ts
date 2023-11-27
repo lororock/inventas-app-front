53b88dfa2b06e49a2afc152a29c8ef8309aaac4e
ernesto diaz <ernesto.diaz@miliopay.com>
2023-11-27 09:41:50 -0500

    :sparkles: create component with logic for panel for actions in dashboard

--- src/assets/list.items.ts
+++ src/assets/list.items.ts
@@ -31,3 +31,62 @@ export const genders = [
     label: "Femenino",
   },
 ];
+
+export const functionsDashboard = [
+  {
+    rol: 1,
+    title: "Modulo de compras",
+    color: "primary",
+    items: [
+      { id: 1, type: 2, icon: "mdi-cart-plus", title: "Registro Compras" },
+      { id: 2, type: 2, icon: "mdi-account-group", title: "Proveedores" },
+      { id: 3, type: 2, icon: "mdi-cash-minus", title: "Cuentas por pagar" },
+    ]
+  },
+  {
+    rol: 1,
+    title: "Modulo de ventas",
+    color: "success",
+    items: [
+      { id: 4, type: 2, icon: "mdi-cash-plus", title: "Registro Ventas" },
+      { id: 5, type: 2, icon: "mdi-account-group", title: "Clientes" },
+      { id: 6, type: 2, icon: "mdi-cash-plus", title: "Cuentas por cobrar" },
+      { id: 7, type: 2, icon: "mdi-currency-eth", title: "Movimientos" },
+    ]
+  },
+  {
+    rol: 1,
+    title: "Modulo de inventario",
+    color: "orange",
+    items: [{ id: 8, type: 2, icon: "mdi-barcode", title: "Registro Productos" },
+      { id: 9, type: 2, icon: "mdi-shape", title: "Categorias" },
+      { id: 10, type: 2, icon: "mdi-text-short", title: "Marcas" },
+      { id: 11, type: 2, icon: "mdi-warehouse", title: "Bodegas" },
+      { id: 12, type: 2, icon: "mdi-share-all-outline", title: "Movimientos" },]
+  },
+  {
+    rol: 1,
+    title: "Modulo de usuarios",
+    color: "purple",
+    items: [
+      {
+        id: 13,
+        type: 2,
+        icon: "mdi-account-multiple",
+        title: "Registro empleados",
+      },
+      {
+        id: 14,
+        type: 2,
+        icon: "mdi-account-cog",
+        title: "Actualizar empleados",
+      },
+      {
+        id: 15,
+        type: 2,
+        icon: "mdi-cash-register",
+        title: "Cajas",
+      },
+    ]
+  },
+];
---
+++ src/components/admin/PanelLateral.vue
@@ -0,0 +1,21 @@
+<template>
+      <v-navigation-drawer v-model="nav" relative app expand-on-hover>
+        <v-alert :title="item.title" density="compact" v-for="(item, index) of listItems" :key="index" :color="item.color">
+          <v-list
+              v-for="(subItem, subIndex) of item.items"
+              :key="subIndex"
+              link
+          >
+            <v-btn variant="text" size="small" :prepend-icon="subItem.icon" >{{subItem.title}}</v-btn>
+          </v-list>
+        </v-alert>
+      </v-navigation-drawer>
+</template>
+
+<script setup lang="ts">
+import {ref} from "vue"
+import {functionsDashboard} from "../../assets/list.items.ts";
+
+const listItems = ref(functionsDashboard)
+let nav = ref(true)
+</script>
