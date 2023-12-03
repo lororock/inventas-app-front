2c686bd69b98e64b8722726ab044060a6b7c143b
ernesto diaz <ernesto.diaz@miliopay.com>
2023-12-03 18:33:32 -0500

    :sparkles: create new component when implement logic for crud operations

---
+++ src/components/general/ITable.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import EntityConfig from "../../interface/entity.config.ts";
+import useCrudStore from "../../store/crud.store.ts";
+
+const props = defineProps({
+  config: { type: Object as () => EntityConfig, required: true },
+});
+
+const crudStore = useCrudStore(props.config)();
+
+const listItem = async () => {
+  try {
+    const result = await crudStore.findAll();
+    console.log(result);
+  } catch (error) {
+    console.error();
+  }
+};
+
+(async () => {
+  await listItem();
+})();
+</script>
+
+<template>
+  <v-container>
+    <v-card>{{ config }}</v-card>
+  </v-container>
+</template>
