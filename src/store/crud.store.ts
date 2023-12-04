b40b945a23fb8b6feb8afad59e54b938ac3b673a
ernesto diaz <ernesto.diaz@miliopay.com>
2023-12-03 22:59:32 -0500

    :sparkles: required page and limit for make request

--- src/store/crud.store.ts
+++ src/store/crud.store.ts
@@ -1,11 +1,15 @@
 import { defineStore } from "pinia";
 import entityConfig from "../interface/entity.config.ts";
 import axiosInstance from "../services/axios.service.ts";
+import PaginationInterface from "../interface/pagination.interface.ts";
 
 const useCrudStore = (entityConfig: entityConfig) =>
   defineStore(entityConfig.name, () => {
-    const findAll = async () => {
-      const { data } = await axiosInstance.get(entityConfig.path);
+    const findAll = async ({ page, limit }: PaginationInterface) => {
+      const params = new URLSearchParams();
+      params.append("page", String(page));
+      params.append("limit", String(limit));
+      const { data } = await axiosInstance.get(entityConfig.path, { params });
       return data;
     };
 
