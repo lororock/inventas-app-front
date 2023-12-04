44cfb6a08a4544948c451d4b534fe51473edce22
ernesto diaz <ernesto.diaz@miliopay.com>
2023-12-03 21:01:40 -0500

    :sparkles: added loading with overlay

--- src/components/general/ITable.vue
+++ src/components/general/ITable.vue
@@ -2,6 +2,7 @@
 import EntityConfig from "../../interface/entity.config.ts";
 import useCrudStore from "../../store/crud.store.ts";
 import { ref } from "vue";
+import LoadInProgress from "./LoadInProgress.vue";
 
 const props = defineProps({
   config: { type: Object as () => EntityConfig, required: true },
@@ -9,14 +10,18 @@ const props = defineProps({
 
 const crudStore = useCrudStore(props.config)();
 
-const items = ref<any>([1, 2, 3]);
+const items = ref<any>([]);
+const loading = ref(false);
 
 const listItem = async () => {
+  loading.value = true;
   try {
     const result = await crudStore.findAll();
     items.value = result.items;
   } catch (error) {
     console.error(error);
+  } finally {
+    loading.value = false;
   }
 };
 
@@ -27,6 +32,7 @@ const listItem = async () => {
 
 <template>
   <v-container>
-    <v-card>{{ items }}</v-card>
+    <LoadInProgress v-if="loading" />
+    <v-card v-else>{{ items }}</v-card>
   </v-container>
 </template>
