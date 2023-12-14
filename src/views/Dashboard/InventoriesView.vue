915f699f0794297fad542752bc067c4e3de835a1
ernesto diaz <ernesto.diaz@miliopay.com>
2023-12-13 23:49:04 -0500

    :sparkles: crud inventory complete successfully

--- src/views/Dashboard/InventoriesView.vue
+++ src/views/Dashboard/InventoriesView.vue
@@ -1,16 +1,15 @@
 <script setup lang="ts">
 import ITable from "../../components/general/ITable.vue";
 import EntityConfig from "../../interface/entity.config.ts";
+import FormInvetory from "../../components/admin/inventories/FormInvetory.vue";
 const configTable: EntityConfig = {
   name: "Inventory",
   path: "inventories",
   columns: [
     { title: "Detalles", key: "id", sortable: false },
     { title: "Localización", key: "location", sortable: false },
-    { title: "Estado", key: "status", sortable: false },
-    { title: "Acciones", key: "actions", sortable: false },
   ],
-  formComponent: null,
+  formComponent: FormInvetory,
 };
 </script>
 
