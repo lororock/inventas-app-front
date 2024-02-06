ae8a81fd8993ef8b9ba80592dd8e59862d900654
ernesto diaz <ernesto.diaz@miliopay.com>
2024-02-05 23:12:01 -0500

    :sparkles: added template field in table

--- src/components/admin/inventories/ListProductForInventory.vue
+++ src/components/admin/inventories/ListProductForInventory.vue
@@ -2,21 +2,45 @@
 import { ref } from "vue";
 import useCrudStore from "../../../store/crud.store.ts";
 import EntityConfig from "../../../interface/entity.config.ts";
+import { Ref } from "@vue/reactivity";
 
 const props = defineProps({
   config: { type: Object as () => EntityConfig, required: true },
   inventoryId: { type: String, required: true },
 });
 const dialog = ref<boolean>(false);
-const inventory = ref<any>(undefined);
+const inventory = ref<any>();
 const loading = ref(false);
+
+const headers = ref([
+  { title: "Detalles", key: "id", sortable: false },
+  { title: "Cantidad", key: "quantity", sortable: false },
+  { title: "Acciones", key: "actions", sortable: false },
+]);
 
 const crudStore = useCrudStore(props.config)();
 
 const findInventory = async () => {
   loading.value = !loading.value;
-  inventory.value = await crudStore.findById(props.inventoryId);
+  const result = await crudStore.findById(props.inventoryId);
+  result.productInventories = result.productInventories.map(
+    ({ product, quantity }: any) => ({
+      id: product.id,
+      name: product.name,
+      quantity,
+    }),
+  );
+  inventory.value = result;
   loading.value = !loading.value;
+};
+
+const validateNumberInput = (event: any, item: any) => {
+  event.preventDefault();
+  const { value } = event.target;
+  console.log(value);
+
+  if (isNaN(+value)) item.quantity = 0;
+  else item.quantity = value;
 };
 </script>
 
@@ -33,7 +57,44 @@ const findInventory = async () => {
           Actualizar inventario <v-icon icon="mdi-list-status" />
         </v-btn>
       </template>
-      <v-card :loading="loading" :title="inventory && inventory.location">
+      <v-card v-if="inventory" :loading="loading" :title="inventory?.location">
+        <v-card-item>
+          <v-data-table
+            :headers="headers"
+            :items="inventory?.productInventories"
+          >
+            <template v-slot:item.id="{ item }">
+              <v-btn color="info" variant="text" @click="console.log(item.id)">
+                {{ item.name }} <v-icon icon="mdi-open-in-new" />
+              </v-btn>
+            </template>
+            <template v-slot:item.quantity="{ item }">
+              <v-text-field
+                density="compact"
+                v-model="item.quantity"
+                @input.prevent="validateNumberInput($event, item)"
+                single-line
+                variant="outlined"
+                label="cantidad"
+                type="number"
+              />
+            </template>
+            <template v-slot:item.actions="{ item }">
+              <v-btn
+                color="red"
+                icon="mdi-minus"
+                @click="item.quantity = item.quantity - 1"
+                size="x-small"
+              />
+              <v-btn
+                color="info"
+                icon="mdi-plus"
+                @click="item.quantity = item.quantity + 1"
+                size="x-small"
+              />
+            </template>
+          </v-data-table>
+        </v-card-item>
         <v-card-actions>
           <v-spacer />
           <v-btn color="primary" variant="tonal"> Guardar cambios </v-btn>
