36deacc82dee043e39fc0b3a2cc6f07db7abdc8d
ernesto diaz <ernesto.diaz@miliopay.com>
2024-02-25 23:43:18 -0500

    :heavy_plus_sign: install  vue-i18n@next

--- src/main.ts
+++ src/main.ts
@@ -9,10 +9,17 @@ import router from "./router";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
+import es from "./locales/es";
 
 const app = createApp(App);
-
 const vuetify = createVuetify({
+  locale: {
+    locale: "es",
+    messages: { es },
+    rtl: {
+      customLocale: true,
+    },
+  },
   icons: {
     defaultSet: "mdi",
   },
