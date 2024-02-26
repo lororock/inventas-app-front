745afdc00e35f01db507a777e717c986a46c5439
ernesto diaz <ernesto.diaz@miliopay.com>
2024-02-26 00:07:32 -0500

    :heavy_plus_sign: change logic for generate messages

--- src/locales/es.ts
+++ src/locales/es.ts
@@ -1,32 +1,37 @@
-const es = {
-  badge: "Insignia",
-  close: "Cerrar",
-  dataIterator: {
-    noResultsText: "No se encontraron registros correspondientes",
-    loadingText: "Cargando items...",
-  },
-  dataTable: {
-    itemsPerPageText: "Filas por página:",
-    ariaLabel: {
-      sortDescending:
-        ": Ordenado descendente. Activar para quitar la ordenación.",
-      sortAscending: ": Ordenado ascendente. Activar para ordenar descendente.",
-      sortNone: ": No ordenado. Activar para ordenar ascendente.",
+import { es } from "vuetify/locale";
+const esLocale = {
+  $vuetify: {
+    ...es,
+    badge: "Insignia",
+    close: "Cerrar",
+    dataIterator: {
+      noResultsText: "No se encontraron registros correspondientes",
+      loadingText: "Cargando items...",
     },
-    sortBy: "Ordenar por",
-  },
-  dataFooter: {
-    itemsPerPageText: "Items por página:",
-    itemsPerPageAll: "Todos",
-    nextPage: "Página siguiente",
-    prevPage: "Página anterior",
-    firstPage: "Primera página",
-    lastPage: "Última página",
-    pageText: "{0}-{1} de {2}",
-  },
-  datePicker: {
-    itemsSelected: "{0} seleccionado(s)",
+    dataTable: {
+      itemsPerPageText: "Filas por página:",
+      ariaLabel: {
+        sortDescending:
+          ": Ordenado descendente. Activar para quitar la ordenación.",
+        sortAscending:
+          ": Ordenado ascendente. Activar para ordenar descendente.",
+        sortNone: ": No ordenado. Activar para ordenar ascendente.",
+      },
+      sortBy: "Ordenar por",
+    },
+    dataFooter: {
+      itemsPerPageText: "Items por página:",
+      itemsPerPageAll: "Todos",
+      nextPage: "Página siguiente",
+      prevPage: "Página anterior",
+      firstPage: "Primera página",
+      lastPage: "Última página",
+      pageText: "{0}-{1} de {2}",
+    },
+    datePicker: {
+      itemsSelected: "{0} seleccionado(s)",
+    },
   },
 };
 
-export default es;
+export default esLocale;
