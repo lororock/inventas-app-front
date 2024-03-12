e691f9e195865d50cfdec4d83b236fb03b5c0f0f
ernesto diaz <ernesto.diaz@miliopay.com>
2024-03-12 12:26:04 -0500

    :sparkles: create view for list enterprises

---
+++ src/views/Dashboard/EnterprisesView.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import ITable from "../../components/general/ITable.vue";
+import EntityConfig from "../../interface/entity.config.ts";
+const configTable: EntityConfig = {
+  name: "Enterprise",
+  path: "enterprises",
+  columns: [
+    { title: "Estado", key: "status", sortable: false },
+    { title: "Empresa", key: "name", sortable: false },
+    { title: "Dirección", key: "address", sortable: false },
+    { title: "Fecha de creacion", key: "createdAt", sortable: false },
+  ],
+  formComponent: null,
+};
+</script>
+
+<template>
+  <ITable :config="configTable" />
+</template>
--- src/views/FormView.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts">
-import FormRegisterEnterprise from "../components/enterprises/FormRegisterEnterprise.vue";
-</script>
-
-<template>
-  <FormRegisterEnterprise />
-</template>
